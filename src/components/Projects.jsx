bd1e59e4e8af7f5cd83d3a695564733af360449d
Evan Beck <evandrydenbeck@gmail.com>
2022-08-07 23:22:55 -0400

    adding Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
+
+let projects = []
+
+const displayProjectCards = projects.map(project => <ProjectCard {...project}/>)
 
 function Projects() {
   return (
-    <div>Here are some of my projects.</div>
+    <div>
+      <p>Here are some of my projects.</p>
+      {displayProjectCards}
+    </div>
   )
 }
 
